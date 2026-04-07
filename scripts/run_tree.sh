034ea75da37ed1bf3837f0b5c9f7d21c581a99d7
Shamsiiat Abdurakhmanova <sham.abdurakh@gmail.com>
2026-04-07 17:20:02 +0300

    minor

--- scripts/run_tree.sh
+++ scripts/run_tree.sh
@@ -31,7 +31,46 @@ done
 START=$SECONDS
 echo "Starting experiments at $(date)"
 
-PARAM_LIST=(2 10 50 100)
+# PARAM_LIST=(2 10 50 100)
+# # ===============================
+# # Index mapping
+# # ===============================
+# IDX=$SLURM_ARRAY_TASK_ID
+
+# NP=${#PARAM_LIST[@]} # number of elements in the array
+# NS=${#SEEDS[@]}
+
+# # Total jobs = 4 * 10 = 40
+
+# PARAM_IDX=$(( IDX / NS ))
+# SEED_IDX=$(( IDX %  NS ))
+
+# PARAM=${PARAM_LIST[$PARAM_IDX]}
+# SEED=${SEEDS[$SEED_IDX]}
+
+# echo "========================================"
+# echo "Running experiment on GPU"
+# echo "  D       = $PARAM"
+# echo "  seed    = $SEED"
+# echo "  GPU     = $CUDA_VISIBLE_DEVICES"
+# echo "========================================"
+
+# srun python scripts/main.py \
+#             --n_clients 150 \
+#             --n_clusters 3 \
+#             --n_features $PARAM \
+#             --model decision_tree \
+#             --lmbd 0.05 \
+#             --dataset synthetic \
+#             --algo Algorithm2_SKLearn \
+#             --R 200 \
+#             --S 30 \
+#             --fname ${OUT_DIR}/linear_syn_dm/Algorithm2_SKLearn/dt_synthetic_${PARAM}_${SEED}.csv \
+#             --device cpu \
+#             --problem regression \
+#             --seed $SEED
+
+PARAM_LIST=(0.05 0.1 0.2 0.5) # lmbd
 # ===============================
 # Index mapping
 # ===============================
@@ -40,68 +79,26 @@ IDX=$SLURM_ARRAY_TASK_ID
 NP=${#PARAM_LIST[@]} # number of elements in the array
 NS=${#SEEDS[@]}
 
-# Total jobs = 4 * 10 = 40
-
 PARAM_IDX=$(( IDX / NS ))
 SEED_IDX=$(( IDX %  NS ))
 
 PARAM=${PARAM_LIST[$PARAM_IDX]}
 SEED=${SEEDS[$SEED_IDX]}
 
-echo "========================================"
-echo "Running experiment on GPU"
-echo "  D       = $PARAM"
-echo "  seed    = $SEED"
-echo "  GPU     = $CUDA_VISIBLE_DEVICES"
-echo "========================================"
-
 srun python scripts/main.py \
             --n_clients 150 \
             --n_clusters 3 \
-            --n_features $PARAM \
+            --n_features 10 \
             --model decision_tree \
-            --lmbd 0.05 \
+            --lmbd $PARAM \
             --dataset synthetic \
             --algo Algorithm2_SKLearn \
             --R 200 \
             --S 30 \
-            --fname ${OUT_DIR}/linear_syn_dm/Algorithm2_SKLearn/dt_synthetic_${PARAM}_${SEED}.csv \
+            --fname ${OUT_DIR}/linear_syn_lmbd/Algorithm2_SKLearn/dt_synthetic_${PARAM}_${SEED}.csv \
             --device cpu \
             --problem regression \
             --seed $SEED
-
-# for D in "${D_LIST[@]}"; do
-#     for SEED in "${SEEDS[@]}"; do
-#         echo "========================================"
-#         echo "Running experiment: D=$D, seed=$SEED"
-#         echo "========================================"
-
-#         # Construct CSV file path
-#         START_EXP=$SECONDS
-
-#         python scripts/main.py \
-#             --n_clients 150 \
-#             --n_clusters 3 \
-#             --n_features $D \
-#             --model decision_tree \
-#             --lmbd 0.05 \
-#             --dataset synthetic \
-#             --algo Algorithm2_SKLearn \
-#             --R 200 \
-#             --S 30 \
-#             --fname ${OUT_DIR}/linear_syn_dm/Algorithm2_SKLearn/dt_synthetic_${D}_${SEED}.csv \
-#             --device cpu \
-#             --problem regression \
-#             --seed $SEED
-
-#         END_EXP=$SECONDS
-#         ELAPSED_EXP=$(( END_EXP - START_EXP ))
-#         echo "Experiment completed in $ELAPSED_EXP seconds"
-#     done
-# done
-# END=$SECONDS
-# ELAPSED=$(( END - START ))
-# echo "All experiments completed in $ELAPSED seconds"
 
 # S_LIST=(10 20 30 50)
 # for S in "${S_LIST[@]}"; do
